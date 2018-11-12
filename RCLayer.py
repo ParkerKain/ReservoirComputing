c17e5b6477771d30958000fa1bc3ee8d085b1102
Parker Kain <parkerjkain@gmail.com>
2018-11-12 15:07:40 -0500

    Base support for Tensorboard monitoring added

--- RCLayer.py
+++ RCLayer.py
@@ -1,39 +1,65 @@
 import tensorflow as tf
 
 class tfESN(tf.keras.layers.Layer):
-    
+
   def __init__(self, n_reservoir):
+    """Builds any parameters that are not based on the inputs recieved by the layer
+    
+    args:
+        n_reservoir: number of reservoir neurons
+    """
     super(tfESN, self).__init__(trainable = False)    
     self.n_reservoir = n_reservoir
 
   def build(self, inputs):
+    """
+    This is run when the first input is passed to the layer, and finalized initalization.
+    Hence all of the input based weights are defined here.
     
+    args:
+        inputs: The inputs to pass through the layer, typically a tensor
+    
+    """
     self.n_inputs = inputs[0] 
     
     #Initialize the reservoir to reservoir weights
-    #self.W_res = tf.random_normal([self.n_reservoir, self.n_reservoir], mean = 0, stddev = 1, dtype = tf.float32, name = 'WeightsFromReservoir', seed=1234)
-    self.W_res = tf.get_variable("tf_esn/WeightsFromReservoir", [self.n_reservoir, self.n_reservoir], trainable = False)
+    self.W_res = tf.get_variable("tf_esn/WeightsFromReservoir", [self.n_reservoir, self.n_reservoir], trainable = False) * 2 - 1
 
     #Initialize the input to reservoir weights
-    #self.W_in = tf.random_normal([self.n_reservoir, self.n_inputs], mean = 0, stddev = 1, dtype = tf.float32, name = 'WeightsFromInput', seed=5678)
-    #self.B_in = tf.random_normal([self.n_reservoir,1], mean = 0, stddev = 1, dtype = tf.float32, name = 'Bias', seed=9012)
-    self.W_in = tf.get_variable("tf_esn/WeightsFromInput", [self.n_reservoir, self.n_inputs], trainable = False)
-    self.B_in = tf.get_variable("tf_esn/Bias", [self.n_reservoir, 1], trainable = False)
+    self.W_in = tf.get_variable("tf_esn/WeightsFromInput", [self.n_reservoir, self.n_inputs], trainable = False) * 2 - 1
+    self.B_in = tf.get_variable("tf_esn/Bias", [self.n_reservoir, 1], trainable = False) * 2 - 1
 
     #Initialize the output to reservoir weights
-    #self.W_out = tf.random_normal([self.n_reservoir, 1], mean = 0, stddev = 1, dtype = tf.float32, name = 'WeightsFromOutput', seed=3456)
-    self.W_out = tf.get_variable("tf_esn/WeightsFromOutput", [self.n_reservoir, 1], trainable = False)
+    self.W_out = tf.get_variable("tf_esn/WeightsFromOutput", [self.n_reservoir, 1], trainable = False) * 2 - 1
 
     #Initialize the current state
-    self.x = tf.zeros([self.n_reservoir, 1], name = 'States')
-    
+    #self.x = tf.zeros([self.n_reservoir, 1], name = 'tf_esn/States')
+    self.x = tf.get_variable(name="tf_esn/States", shape=[self.n_reservoir, 1], initializer=tf.zeros_initializer())
 
   def call(self, inputs):
-    partInput = tf.reshape(tf.tensordot(self.W_in, inputs, 1),[self.n_inputs, self.n_reservoir])
-    partReservoir = tf.transpose(tf.tensordot(self.W_res, self.x, 1))
-    partBias = tf.transpose(self.B_in)
+    """
+    This handles the actual passing through of the layer
     
-    currentState = (partInput + partReservoir + partBias)
+    args:
+        inputs: the input to pass through the layer
+    """
+    partInput = tf.reshape(tf.tensordot(self.W_in, inputs, 1),[self.n_inputs, self.n_reservoir], name = 'partInput')
+    partReservoir = tf.transpose(tf.tensordot(self.W_res, self.x, 1), name = 'partReservoir')
+    partBias = tf.transpose(self.B_in, name = 'partBias')
+    
+    currentState = tf.add_n((partInput, partReservoir, partBias), 'CurrentState')
+    self.x = currentState
+
+    tf.summary.histogram("partInput", partInput)
+    tf.summary.histogram("partReservoir", partReservoir)
+    tf.summary.histogram("partBias", partBias)    
+    tf.summary.histogram("x", self.x)
+    tf.summary.histogram("currentState", currentState)
+    tf.summary.histogram("WeightsFromInput", self.W_in)
+    tf.summary.histogram("BiasFromInput", self.B_in)
+    tf.summary.histogram("WeightsFromOutput", self.W_out)
+    tf.summary.histogram("WeightsFromReservoir", self.W_res)
+
     
     return currentState
 
--- TFESN2.py
+++ TFESN2.py
@@ -52,21 +52,22 @@ n_readout = 1
 #Define the Data
 #-------------------------------------
 
-lengthTrain = 100
-lengthTest = 1000
+lengthTrain = 1000
+lengthTest = 100
 parity = 2
 n_outputs = parity - 1
 
-(u, y_true) = generateParity(lengthTrain, parity) 
+(u_train, y_train) = generateParity(lengthTrain, parity) 
+(u_test, y_test) = generateParity(lengthTest, parity) 
 
 #-------------------------------------
 #Generate Network
 #-------------------------------------
 
 #Make Iterator
-dataset = tf.data.Dataset.from_tensor_slices(
-            {'u': tf.convert_to_tensor(u, dtype=tf.float32),
-             'y': tf.convert_to_tensor(y_true, dtype=tf.float32)})
+u, y = tf.placeholder(tf.float32, shape=[None,1]), tf.placeholder(tf.float32, shape=[None,1])
+dataset = tf.data.Dataset.from_tensor_slices(({'u': u, 
+                                               'y': y}))    
     
 iterator = dataset.make_initializable_iterator()
 next_row = iterator.get_next()
@@ -81,6 +82,7 @@ readout = tf.layers.Dense(units = 1,
                           activation = tf.tanh,
                           name = 'Readout')
 
+#Get prediction
 y_pred = tf.reshape(readout(currentState),[1,])
 
 #-------------------------------------
@@ -88,26 +90,56 @@ y_pred = tf.reshape(readout(currentState),[1,])
 #-------------------------------------
 with tf.name_scope('Training_Parameters'):
     loss = tf.losses.mean_squared_error(labels=next_row['y'], predictions=y_pred)
-
     #-------------------------------------
-    #Training
+    #Training Measurements
     #-------------------------------------
     optimizer = tf.train.GradientDescentOptimizer(0.01)
     train = optimizer.minimize(loss)
-
-
+    
+#-------------------------------------
+#Initialize Variables
+#-------------------------------------
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-sess.run(iterator.initializer)
+sess.run(iterator.initializer, feed_dict={ u: u_train, y: y_train})
+
+#-------------------------------------
+#Initialize Tensorboard Grpah and Summarise
+#-------------------------------------
+tf.summary.scalar('loss',loss)
+
+merged_summary = tf.summary.merge_all()
+writer = tf.summary.FileWriter(".")
+writer.add_graph(sess.graph)
 
 #sess = tf_debug.LocalCLIDebugWrapperSession(sess) #Debugging
-for i in range(100):
-    _, current_loss = sess.run((train,loss))
-    print(current_loss)
-    
+
 #-------------------------------------
-#Output Graph
+#Training
 #-------------------------------------
-writer = tf.summary.FileWriter('.')
-writer.add_graph(tf.get_default_graph())
+print('Training ...')
+outputsPred = []
+outputsTrain = []
+for i in range(lengthTrain):
+    (summary, _, current_loss, predicted, actual) = sess.run((merged_summary, train, loss, y_pred, next_row['y']))
+    outputsPred.append(predicted)
+    outputsTrain.append(actual)
+    if i >= 100:
+        accuracy = np.sum(np.abs(np.round(np.array(outputsPred) - np.array(outputsTrain)))/ 100)
+        tf.summary.scalar('accuracy',accuracy)
+        outputsPred = outputsPred[1:101]
+        outputsTrain = outputsTrain[1:101]
+    if (i+1) % 1000 == 0:
+        print(round(i / lengthTrain * 100), '% complete')        
+    writer.add_summary(summary, i)
+    #print(current_loss)
+
+#-------------------------------------
+#Testing
+#-------------------------------------
+print('Switching to Test ... ')
+sess.run(iterator.initializer, feed_dict={u: u_test, y: y_test})
+for i in range(lengthTest):
+    test_predicted = sess.run((y_pred))
+    #print(test_predicted)
