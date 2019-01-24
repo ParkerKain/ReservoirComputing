6d68edd0537bbc88a2c10aa6445eec18d3ab2da3
Parker Kain <parkerjkain@gmail.com>
2019-01-23 19:10:15 -0500

    added GRU layer

--- GPU Test.py
+++ GPU Test.py
@@ -30,60 +30,62 @@ parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--train_steps', default=1000, type=int,
                     help='number of training steps')
 
+
 def main(argv):
-    args = parser.parse_args(argv[1:])
-
-    # Fetch the data
-    (train_x, train_y), (test_x, test_y) = irisgenerate.load_data()
-
-    # Feature columns describe how to use the input.
-    my_feature_columns = []
-    for key in train_x.keys():
-        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
-
-    # Build 2 hidden layer DNN with 10, 10 units respectively.
-    classifier = tf.estimator.DNNClassifier(
-        feature_columns=my_feature_columns,
-        # Two hidden layers of 10 nodes each.
-        hidden_units=[10000, 10000],
-        # The model must choose between 3 classes.
-        n_classes=3)
-
-    # Train the Model.
-    classifier.train(
-        input_fn=lambda:irisgenerate.train_input_fn(train_x, train_y,
-                                                 args.batch_size),
-        steps=args.train_steps)
-
-    # Evaluate the model.
-    eval_result = classifier.evaluate(
-        input_fn=lambda:irisgenerate.eval_input_fn(test_x, test_y,
-                                                args.batch_size))
-
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
-
-    # Generate predictions from the model
-    expected = ['Setosa', 'Versicolor', 'Virginica']
-    predict_x = {
-        'SepalLength': [5.1, 5.9, 6.9],
-        'SepalWidth': [3.3, 3.0, 3.1],
-        'PetalLength': [1.7, 4.2, 5.4],
-        'PetalWidth': [0.5, 1.5, 2.1],
-    }
-
-    predictions = classifier.predict(
-        input_fn=lambda:irisgenerate.eval_input_fn(predict_x,
-                                                labels=None,
-                                                batch_size=args.batch_size))
-
-    template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
-
-    for pred_dict, expec in zip(predictions, expected):
-        class_id = pred_dict['class_ids'][0]
-        probability = pred_dict['probabilities'][class_id]
-
-        print(template.format(irisgenerate.SPECIES[class_id],
-                              100 * probability, expec))
+    with tf.device('/gpu:0'):
+        args = parser.parse_args(argv[1:])
+    
+        # Fetch the data
+        (train_x, train_y), (test_x, test_y) = irisgenerate.load_data()
+    
+        # Feature columns describe how to use the input.
+        my_feature_columns = []
+        for key in train_x.keys():
+            my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+    
+        # Build 2 hidden layer DNN with 10, 10 units respectively.
+        classifier = tf.estimator.DNNClassifier(
+            feature_columns=my_feature_columns,
+            # Two hidden layers of 10 nodes each.
+            hidden_units=[1000, 1000],
+            # The model must choose between 3 classes.
+            n_classes=3)
+    
+        # Train the Model.
+        classifier.train(
+            input_fn=lambda:irisgenerate.train_input_fn(train_x, train_y,
+                                                     args.batch_size),
+            steps=args.train_steps)
+    
+        # Evaluate the model.
+        eval_result = classifier.evaluate(
+            input_fn=lambda:irisgenerate.eval_input_fn(test_x, test_y,
+                                                    args.batch_size))
+    
+        print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+    
+        # Generate predictions from the model
+        expected = ['Setosa', 'Versicolor', 'Virginica']
+        predict_x = {
+            'SepalLength': [5.1, 5.9, 6.9],
+            'SepalWidth': [3.3, 3.0, 3.1],
+            'PetalLength': [1.7, 4.2, 5.4],
+            'PetalWidth': [0.5, 1.5, 2.1],
+        }
+    
+        predictions = classifier.predict(
+            input_fn=lambda:irisgenerate.eval_input_fn(predict_x,
+                                                    labels=None,
+                                                    batch_size=args.batch_size))
+    
+        template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
+    
+        for pred_dict, expec in zip(predictions, expected):
+            class_id = pred_dict['class_ids'][0]
+            probability = pred_dict['probabilities'][class_id]
+    
+            print(template.format(irisgenerate.SPECIES[class_id],
+                                  100 * probability, expec))
 
 
 if __name__ == '__main__':
