8a25a55f261dc120a461e5072ca7a7a4ab42a164
ParkerKain <parkerjkain@gmail.com>
2019-02-20 17:59:28 -0500

    Pushing current work on batch processing

--- RCLayer.py
+++ RCLayer.py
@@ -10,21 +10,30 @@ import tensorflow as tf
 class tfESN(tf.keras.layers.Layer):
 
   def __init__(self, n_reservoir, 
+                       batch_size,
+                       n_inputs,
                        teacher_shift = 0, 
-                       teacher_scale = 1):
+                       teacher_scale = 1,
+                       prev_state_weight = 0):
     """
     Builds any parameters that are not based on the inputs recieved by the layer
     
     args:
         n_reservoir: number of reservoir neurons
+        batch_size: number of inputs to pass into the network at a given timestep
         teacher_shift: amount to shift the true outputs that are fed back into the reservoir
         teacher_scale: amount to scale the true outputs that are fed back into the reservoir
+        prev_state_weight: amount to weight the preious weight when calculating the current weight. Defaults to 0.
+        n_inputs: number of inputs passed at one time 
     """
-    super(tfESN, self).__init__(trainable = False)    
+    super(tfESN, self).__init__(trainable = False)   
+    self.batch_size = batch_size
     self.n_reservoir = n_reservoir
     self.teacher_shift = teacher_shift
     self.teacher_scale = teacher_scale
-
+    self.prev_state_weight = prev_state_weight
+    self.n_inputs = n_inputs
+    
   def build(self, inputs):
     """
     This is run when the first input is passed to the layer, and finalized initalization.
@@ -36,33 +45,53 @@ class tfESN(tf.keras.layers.Layer):
                 timestep's true output.
     """
     
-    passed = inputs[0]
+    # ------------------------------------------------------
+    # Finish Defining the Reservoir
+    # -------------------------------------------------------
+
     self.spectral_radius = 0.95
     self.n_outputs = inputs[1][0]
-    self.n_inputs = 1 #ONLY WORKS FOR PARITY
+    self.n_inputs = self.n_inputs 
     
     #Initialize the reservoir to reservoir weights
     self.W_res = tf.get_variable("tf_esn/WeightsFromReservoir", [self.n_reservoir, self.n_reservoir], 
                                  initializer = tf.initializers.random_uniform(minval = -0.5, maxval=0.5),trainable = False)
-    
+        
     # compute the spectral radius of these weights:
     (e,v) = tf.linalg.eigh(self.W_res)
     radius = tf.reduce_max(tf.abs(e))
     # rescale them to reach the requested spectral radius:
     self.W_res = self.W_res * (self.spectral_radius / radius)
 
+    print('Reservoir to Reservoir weights:', self.W_res.shape)
+
     #Initialize the input to reservoir weights
+    #self.W_in = tf.get_variable("tf_esn/WeightsFromInput", [self.n_reservoir, self.n_inputs], 
+    #                            initializer = tf.initializers.random_uniform(minval = -1, maxval=1), trainable = False)
     self.W_in = tf.get_variable("tf_esn/WeightsFromInput", [self.n_reservoir, self.n_inputs], 
                                 initializer = tf.initializers.random_uniform(minval = -1, maxval=1), trainable = False)
+    
+    print('Input to Reservoir weights:', self.W_in.shape)
+    
     self.B_in = tf.get_variable("tf_esn/Bias", [self.n_reservoir, 1], trainable = False)
+
+    print('Bias to Reservoir weights:', self.B_in.shape)
 
     #Initialize the output to reservoir weights
     self.W_out = tf.get_variable("tf_esn/WeightsFromOutput", [self.n_reservoir, self.n_outputs], 
                                  initializer = tf.initializers.random_uniform(minval = -1, maxval=1), trainable = False)
 
+    print('Output to Reservoir weights:', self.W_out.shape)
+
     #Initialize the current state
-    #self.x = tf.zeros([self.n_reservoir, 1], name = 'tf_esn/States')
-    self.x = tf.get_variable(name="tf_esn/States", shape=[self.n_reservoir, 1], initializer=tf.zeros_initializer())
+    self.x = [tf.get_variable(name = ('state' + str(i)),shape=[self.n_reservoir, 1], initializer=tf.zeros_initializer()) for i in range(self.batch_size)]
+    #self.x = tf.get_variable(name="tf_esn/States", shape=[self.n_reservoir, 1], initializer=tf.zeros_initializer())
+
+    print('States:', self.x)
+
+    #Initialize a variable to store previous state
+    self.prev_state = [tf.reshape(self.x[i], [1, self.n_reservoir]) for i in range(self.batch_size)]
+    #self.prev_state = tf.reshape(self.x, [1, self.n_reservoir])
 
   def call(self, inputs):
     """
@@ -73,30 +102,60 @@ class tfESN(tf.keras.layers.Layer):
                 the first element being the input to pass through the reservoir, and the second 
                 being the previous timestep's true output.
     """
-    
+    #Set up passed in inputs to be correct shapes
     passed = inputs[0]
+    print('---------------------------------------------------------')
+    print('Passed before reshaping', passed.shape)
+    passed = tf.reshape(passed, [self.batch_size,self.n_inputs])
+    print('Passed After Reshaping', passed.shape)
 
+    #Scale previous output
     lastOutput = inputs[1] * self.teacher_scale + self.teacher_shift
+    #Set up variables that will keep hold of states and outputs as they accrue below
+    currentStates = []
     
-    partInput = tf.reshape(tf.tensordot(self.W_in, passed, 1),[1, self.n_reservoir], name = 'partInput')
-    partReservoir = tf.transpose(tf.tensordot(self.W_res, self.x, 1), name = 'partReservoir')
-    partOutput = tf.reshape(tf.tensordot(self.W_out, lastOutput, 1),[1, self.n_reservoir], name = 'partOutput')
-    partBias = tf.transpose(self.B_in, name = 'partBias')
-    
-    
-    currentState = tf.tanh(tf.add_n((partInput, partReservoir, partOutput, partBias)), 'CurrentState')
-    self.x = currentState
+    print('uPassed', inputs[0])
+    print('yLag', lastOutput)
 
-    tf.summary.histogram('passed', passed)
-    tf.summary.histogram("partInput", partInput)
-    tf.summary.histogram("partReservoir", partReservoir)
-    tf.summary.histogram("partOutput", partOutput)
-    tf.summary.histogram("partBias", partBias)    
-    tf.summary.histogram("x", self.x)
-    tf.summary.histogram("currentState", currentState)
-    tf.summary.histogram("WeightsFromInput", self.W_in)
-    tf.summary.histogram("BiasFromInput", self.B_in)
-    tf.summary.histogram("WeightsFromOutput", self.W_out)
-    tf.summary.histogram("WeightsFromReservoir", self.W_res)
     
-    return currentState
+    #Loop for batches, grab each input, pass it through the reservoir, append outputs.
+    for i in range(self.batch_size):
+        currentPassed = passed[i]
+        currentPrevious = lastOutput[:,i]
+        print('---------------------------------------------------------')
+        print('currentPassed',currentPassed.shape)
+        print('currentPrevious',currentPrevious.shape)
+        print('state:', self.x)
+        
+        partInput = tf.reshape(tf.tensordot(self.W_in, currentPassed, 1),[1, self.n_reservoir], name = 'partInput')
+        print('partInput', partInput.shape)
+        partReservoir = tf.transpose(tf.tensordot(self.W_res, self.x[i], 1), name = 'partReservoir')
+        print('partReservoir', partReservoir.shape)
+        partOutput = tf.reshape(tf.tensordot(self.W_out, currentPrevious, 1),[1, self.n_reservoir], name = 'partOutput')
+        print('partOutput', partOutput.shape)
+        partBias = tf.transpose(self.B_in, name = 'partBias')
+        print('partBias', partBias.shape)
+        partPrevious = (tf.transpose(self.x[i]))
+        print('partPrevious', partPrevious.shape)
+        
+        currentState = (self.prev_state_weight * partPrevious) + ((1-self.prev_state_weight)*tf.tanh(tf.add_n((partInput, partReservoir, partOutput, partBias)), 'CurrentState'))
+        print('currentState', currentState.shape)
+        self.x[i] = currentState
+        currentStates.append(currentState)
+        print('List of States:',currentStates)
+        
+        tf.summary.histogram('passed', passed)
+        tf.summary.histogram("partInput", partInput)
+        tf.summary.histogram("partReservoir", partReservoir)
+        tf.summary.histogram("partOutput", partOutput)
+        tf.summary.histogram("partBias", partBias)    
+        #tf.summary.histogram("x", self.x)
+        tf.summary.histogram("currentState", currentState)
+        tf.summary.histogram("WeightsFromInput", self.W_in)
+        tf.summary.histogram("BiasFromInput", self.B_in)
+        tf.summary.histogram("WeightsFromOutput", self.W_out)
+        tf.summary.histogram("WeightsFromReservoir", self.W_res)
+        
+    #return currentState
+    return currentStates
+
--- TFESN2.py
+++ TFESN2.py
@@ -3,61 +3,136 @@
 # This code is a framework for testing sequential parity on various neural net architectures.
 # Here, we use a reservoir layer created by RCLayer.py followed by a dense layer.
 # Changing parity length will allow the user to see how the framework scales to more complicated tasks.
-
+import time 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 import tensorflow as tf
 import numpy as np
 from RCLayer import tfESN
 from tensorflow.python import debug as tf_debug
 
+
 tf.reset_default_graph()
 
 
-def generateParity(length, parity):
+# =============================================================================
+# def generateParity(length, parity):
+#     """Generates a (length) sized bitstring of (parity) parity 
+#     
+#     Args:
+#         length: the length of the bitrstring to return
+#         parity: parity to measure
+#     Returns:
+#         u: a bitstring of (length) size, randomly generated
+#         y: a length_by_(parity)-1 numpy array of parity results. 
+#         y_lag : same as above, but offput by one timestep
+#     """
+#     u = np.random.randint(2, size=length).reshape(length,1)
+#     y = []
+#     currentParity = 0 
+#     
+#     for i in range(length):
+#         parityState = np.zeros(parity)   
+#     
+#         currentU = u[i]
+#         currentParity = (currentParity + currentU) % parity
+#         parityState = np.zeros((1, parity)) 
+#         parityState[:,currentParity] = 1
+#     
+#         y.append(parityState)
+#     y = np.array(y).reshape(length, parity)
+#     y_lag = np.append(np.array([0]), y)[:-1].reshape(length, parity)
+#     return((u, y, y_lag))
+
+
+def generateParity(length, parity, numStrings = 1):
     """Generates a (length) sized bitstring of (parity) parity 
     
     Args:
         length: the length of the bitrstring to return
         parity: parity to measure
+        numStrings: number of bitstrings to create
     Returns:
         u: a bitstring of (length) size, randomly generated
-        y: a length_by_(parity)-1 numpy array of parity results. 
+        y: a length_by_(parity)-1_by_numStrings numpy array of parity results. 
+        y_lag : same as above, but offput by one timestep
     """
-    u = np.random.randint(2, size=length).reshape(length,1)
+    totalU = []
+    
+    for i in range(numStrings):
+        u = np.random.randint(2, size=length)
+        totalU.append(u)
+        
+    totalU = (np.array(totalU))
+
+    print('totalU Original', totalU.shape)
+
+    totalU = np.transpose(totalU, (1,0))
+    print('totalU tranposed', totalU.shape)
+    
     y = []
+    #totalY = np.zeros([length, parity, numStrings])
+    totalY = []
     currentParity = 0 
     
-    for i in range(length):
-        
-        if parity == 2:
-            parityState = np.array([0])
-        else:
-            parityState = np.zeros(parity-1)   
-        
-        currentU = u[i]
-        currentParity = (currentParity + currentU) % parity
-        
-        parityState = np.zeros((1, parity-1)) 
-        if currentParity != 0:
-            parityState[:,currentParity-1] = 1
-        
-        parityState = parityState
-        y.append(parityState)
-    y = np.array(y).reshape(length, parity-1)
-    y_lag = np.append(np.array([0]), y)[:-1].reshape(length, parity-1)
-    return((u, y, y_lag))
+    for string in range(numStrings):
+        #print(string)
+        for i in range(length):
+            #print('\ni =', i)
+            parityState = np.zeros(parity)   
+            
+            currentU = totalU[i, string]
+            
+            #print('currentU:', currentU)
+            currentParity = (currentParity + currentU) % parity
+            
+            #print('currentParity', currentParity)
+            
+            parityState = np.zeros((1, parity)) 
+            parityState[:,currentParity] = 1
 
+            #print('parityState:', parityState)
+            
+            
+            parityState = parityState
+            y.append(parityState)
+        #y = np.array(y).reshape(length, parity)
+        y = np.squeeze(np.array(y))
+        #totalY[:,:,string] = y
+        totalY.append(y)
+        y = []
+        currentParity = 0
+        
+    print('totalY', np.array(totalY).shape)
+    print('totalY reshaped', np.transpose(np.array(totalY), (1,2,0)).shape)
+    totalY = np.transpose(np.array(totalY), (1,2,0))
+    
+    print('y_lag test!', (np.append(np.zeros((1,parity,numStrings)), totalY, axis = 0)[:-1]).shape)
+    y_lag = np.append(np.zeros((1,parity,numStrings)), totalY, axis = 0)[:-1]
+    #y_lag = np.append(np.array([0]), totalY)[:-1].reshape(length, parity, numStrings)
+    print('y_lag', y_lag.shape)
+    #totalY = np.array(totalY).reshape(length, parity, numStrings)
+    return((totalU, totalY, y_lag))
+
+
+# =============================================================================
 #---------------------------------------------------------------------------
 #Hyperparameters 
 #---------------------------------------------------------------------------
+    
 #tf.set_random_seed(1234)
-lengthTrain = 1000
+readoutLayer = "Dense"
+batch_size = 1
+numStrings = 1
+lengthTrain = 10000
 lengthTest = 1000
-parity = 2
+parity = 20
 teacher_shift = -0.7
 teacher_scaling = 1.12
 n_inputs = 1
-n_outputs = parity - 1
-n_reservoir = 50
+n_outputs = parity
+n_reservoir = 20
+prev_state_weight = 0.1
 output_activation = tf.tanh
 train_file_location = "C:/Users/kainp1/Documents/GitHub/ReservoirComputing/training"
 test_file_location = "C:/Users/kainp1/Documents/GitHub/ReservoirComputing/testing"
@@ -65,51 +140,82 @@ test_file_location = "C:/Users/kainp1/Documents/GitHub/ReservoirComputing/testin
 #---------------------------------------------------------------------------
 #Define the Data
 #---------------------------------------------------------------------------
+start_time = time.time()
+(u_train, y_train, y_lag_train) = generateParity(lengthTrain, parity, numStrings)
+print('parity:', parity)
+print('batch_size:', batch_size) 
+print('Input shape', u_train.shape)
+print('Output shape', y_train.shape)
 
-(u_train, y_train, y_lag_train) = generateParity(lengthTrain, parity) 
-(u_test, y_test, y_lag_test) = generateParity(lengthTest, parity) 
-
+#(u_test, y_test, y_lag_test) = generateParity(lengthTest, parity, numStrings) 
 
 #-------------------------------------
 #Generate Network
 #-------------------------------------
+
 #Make Iterator
-u, y, y_lag = tf.placeholder(tf.float32, shape=[None,1]), tf.placeholder(tf.float32, shape=[None,n_outputs]), tf.placeholder(tf.float32, shape=[None,n_outputs])
+u, y, y_lag = (tf.placeholder(tf.float32, shape=[None,batch_size]), 
+              tf.placeholder(tf.float32, shape=[None,n_outputs,batch_size]), 
+              tf.placeholder(tf.float32, shape=[None,n_outputs,batch_size]))
+
 dataset = tf.data.Dataset.from_tensor_slices(({'u': u, 
                                                'y': y,
-                                               'y_lag':y_lag}))    
+                                               'y_lag':y_lag}))
+    
 iterator = dataset.make_initializable_iterator()
 next_row = iterator.get_next()
 
+print('next_row["u"] from iterator:', next_row['u'].shape)
+print('next_row["y"] from iterator:', next_row['y'].shape)
+
 #Make Reservoir
-my_ESN = tfESN(n_reservoir, teacher_shift, teacher_scaling)
+my_ESN = tfESN(n_reservoir, batch_size, n_inputs, teacher_shift, teacher_scaling, prev_state_weight)
+
+print('---------------------------------------------------------')
+print('Entering Reservoir')
 currentState = my_ESN((next_row['u'], next_row['y_lag']))       
+print('---------------------------------------------------------')
+print('Exiting Reservoir')
+combinedStates = tf.concat(currentState, axis = 0)
+#Readout initializing
+if readoutLayer == "Dense":
+    
+    print('combinedStates', combinedStates.shape)
 
-#Readout - Dense
-#readout = tf.layers.Dense(units = n_outputs, 
-#                          use_bias = True,
-#                          activation = output_activation,
-#                         name = 'ReadoutDense')
+    readout = tf.layers.Dense(units = n_outputs, 
+                              use_bias = True,
+                              activation = output_activation,
+                             name = 'ReadoutDense')
+    y_preds = readout(combinedStates)
+    print('Readout', y_preds)
+    
+    #y_preds = []
+    #for i in range(batch_size):
+    #    y_preds.append(tf.reshape(readout(currentState[i]),[parity,1]))
 
-#Readout - GRU
-readout = tf.keras.layers.GRU(units = n_outputs,
-                              name = 'ReadoutGRU')
-
-#Get prediction
-print(currentState)
-y_pred = tf.reshape(readout(tf.reshape(currentState, [1,50,1])),[n_outputs,])
+elif readoutLayer == "GRU":
+    readout = tf.keras.layers.GRU(units = n_outputs,
+                                  name = 'ReadoutGRU')
+    
+    Transformed_3D = readout(tf.reshape(currentState, [1,n_reservoir,1]))
+    y_pred = tf.reshape(Transformed_3D,[n_outputs,batch_size])
 
 #-------------------------------------
 #Loss
 #-------------------------------------
 with tf.name_scope('Training_Parameters'):
-    loss = tf.losses.mean_squared_error(labels=next_row['y'], predictions=y_pred)
+    loss = tf.losses.mean_squared_error(labels=next_row['y'], predictions=tf.reshape(y_preds, [parity, batch_size]))
+    #loss = tf.losses.mean_squared_error(labels=next_row['y'], predictions=tf.transpose(y_preds, (1,0)))
+
+    #print(loss)
     #-------------------------------------
     #Training Measurements
     #-------------------------------------
     optimizer = tf.train.AdamOptimizer()
     train = optimizer.minimize(loss)
-    acc, acc_op = tf.metrics.accuracy(tf.round(next_row['y']), tf.round(y_pred), name = 'accuracy')
+    acc, acc_op = tf.metrics.accuracy(tf.round(next_row['y']), tf.round(tf.reshape(y_preds, [parity, batch_size])), name = 'accuracy')
+    #acc, acc_op = tf.metrics.accuracy(tf.round(tf.reshape(next_row['y'][:,0],[2,1])), tf.round(y_pred), name = 'accuracy')
+    running_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope = "accuracy")
     
 #-------------------------------------
 #Initialize Variables
@@ -118,7 +224,6 @@ init_g = tf.global_variables_initializer()
 init_l = tf.local_variables_initializer()
 config = tf.ConfigProto(allow_soft_placement = True)
 sess = tf.Session(config = config)
-#sess = tf.Session()
 sess.run(init_g)
 sess.run(init_l)
 sess.run(iterator.initializer, feed_dict={u: u_train, y: y_train, y_lag:y_lag_train})
@@ -140,18 +245,53 @@ writer.add_graph(sess.graph)
 #-------------------------------------
 #Training
 #-------------------------------------
+preds = []
 print('Training ...')
 for i in range(lengthTrain):
     #Run one timestep
-    (summary, _, current_loss, cur_acc_op, cur_acc) = sess.run((merged_summary, train, loss, acc_op, acc))
+    (summary, _, current_loss, cur_acc_op, cur_acc, predictions) = sess.run((merged_summary, train, loss, acc_op, acc, y_preds))
+    preds.append(predictions)
+    #print('------------------------------------------')
+    #print('prediction shape')
+    #print(np.array(predictions).shape)
+    #print('overall prediction list shape')
+    #print(np.array(preds).shape)
+    #print('Before reshaping')
+    #print(np.array(predictions))
+    #print('After reshaping')
+    #print(np.array(predictions).reshape(1, 3, 2))
     
     #Give status update
     if (i+1) % 1000 == 0:
         print(round(i / lengthTrain * 100), '% complete')  
     
     #Write summary to tensorboard
-    if i % 100 == 0:
+    if (i % 100 == 0) and (i != 0):
+        
+        #Calculate and write hamming accuracy!
+        
+        last100Preds = np.transpose(np.array(preds[i-100:i]), (1,2,0))
+        print(last100Preds.shape)
+        #last100Preds = last100Preds.reshape([100,parity, batch_size])
+        oneHotPreds = (last100Preds == last100Preds.max(axis=1)[:,None]).astype(int)
+        print(oneHotPreds[:,:,0])
+        #print(oneHotPreds.shape)
+        last100True = np.transpose(np.array(y_train[i-100:i]), (2,1,0))
+        #print(last100True.shape)
+        print(last100True[:,:,0])
+        
+        #print(oneHotPreds)
+        #print(last100True[0])
+        #print(oneHotPreds[0] - last100True[0])
+        
+        print('----------------------------------')
+        #print(np.abs(oneHotPreds - last100True))
+        #print(np.sum(np.abs(oneHotPreds - last100True)))
+        hammingAcc = 1 - np.sum(np.abs(oneHotPreds - last100True)) / parity / 100
+        print('Accuracy for last 100:', hammingAcc * 100, '%')
+
         writer.add_summary(summary, i)
+
 
 #-------------------------------------
 #Testing
@@ -162,14 +302,28 @@ print('Switching to Test ... ')
 #Reinitialize data with test set
 sess.run(iterator.initializer, feed_dict={u: u_test, y: y_test, y_lag:y_lag_test})
 
+preds = []
 for i in range(lengthTest):
     #Do a single passthrough
-    (summary, _, current_loss, cur_acc_op, cur_acc) = sess.run((merged_summary, train, loss, acc_op, acc))
+    (summary, _, current_loss, cur_acc_op, cur_acc, predictions) = sess.run((merged_summary, train, loss, acc_op, acc, y_pred))
+    preds.append(predictions)
     
     #Status update
     if (i+1) % 1000 == 0:
         print(round(i / lengthTest * 100), '% complete') 
     
     #Write to tensorboard
-    if i % 100 == 0:
+    if (i % 100 == 0) and (i != 0):
+        
+        #Calculate and write hamming accuracy!
+        last100Preds = np.array(preds[i-100:i])
+        oneHotPreds = (last100Preds == last100Preds.max(axis=1)[:,None]).astype(int)
+        last100True = np.array(y_test[i-100:i])
+        
+        
+        hammingAcc = 1 - np.sum(np.abs(oneHotPreds - last100True)) / 2 / 100
+        print('Accuracy for last 100:', hammingAcc * 100, '%')
+
         test_writer.add_summary(summary, i)
+
+print(time.time() - start_time)
