12eead18c19aaa4df4d3d35b922c85374f3cf284
ParkerKain <parkerjkain@gmail.com>
2019-02-28 21:12:45 -0500

    Split things into multiple functions

--- parkinsonsExplore.py
+++ parkinsonsExplore.py
@@ -9,43 +9,101 @@ import glob
 import pandas as pd
 import numpy as np
  # get data file names
-path =r'C:\Users\kainp1\Documents\GitHub\ReservoirComputing\Parkinsons\hw_dataset\control'
-filenames = glob.glob(path + "/*.txt")
-
-#Load in each text file
-col_Names=["X", "Y", "Z", "Pressure","GripAngle","Timestamp","ID"]
-dfs = []
-for filename in filenames:
-    table = pd.read_table(filename, sep = ';', header = None, names = col_Names)
-    table = table[table.ID == 0]
-    dfs.append(table)
-
-
-#Get longest length so I can zero pad the rest
-lengths = [len(entry) for entry in dfs]
-print(lengths)
-maxLength = max(lengths)
-print(maxLength)
-
-#Perform zero padding
-zeroPadded = []
-for df in dfs:
-    for i in range(maxLength):
-        if i >= len(df):
-            df = df.append({'X' : 0 , 'Y' : 0, 'Z' : 0, 'Pressure' : 0, 
-                            'GripAngle' : 0, 'Timestamp' : 0, 'ID' : 0}, ignore_index = True)
-    zeroPadded.append(df)
-            
-for df in zeroPadded:
-    print(df.shape)
+ 
+def gatherData():
+        
+    print('Gathering ...')
     
-#Convert to numpy array
-numpyToSave = np.array([zeroPadded[i].values for i in range(len(dfs))])
-print(numpyToSave.shape)
-print(type(numpyToSave))
+    path =r'C:\Users\kainp1\Documents\GitHub\ReservoirComputing\Parkinsons\hw_dataset\control'
+    c_filenames = glob.glob(path + "/*.txt")
+    
+    path =r'C:\Users\kainp1\Documents\GitHub\ReservoirComputing\Parkinsons\hw_dataset\parkinson'
+    p_filenames = glob.glob(path + "/*.txt")
+    
+    filenames = c_filenames + p_filenames
+    
+    #Load in each text file
+    col_Names=["X", "Y", "Z", "Pressure","GripAngle","Timestamp","ID"]
+    dfs = []
+    
+    for filename in filenames:
+        if 'P_' in filename:
+            parkinsonsFlag = 1
+        else:
+            parkinsonsFlag = 0
+            
+        table = pd.read_table(filename, sep = ';', header = None, names = col_Names)
+        table = table[table.ID == 0]
+        table = table.assign(Parkinsons=pd.Series([parkinsonsFlag] * len(table)))
+        dfs.append(table)
+    
+    
+    #Get longest length so I can zero pad the rest
+    lengths = [len(entry) for entry in dfs]
+    print(lengths)
+    maxLength = max(lengths)
+    print(maxLength)
+    
+    #Perform zero padding
+    zeroPadded = []
+    for df in dfs:
+        parkinsonsFlag = max(df['Parkinsons'])
+        for i in range(maxLength):
+            if i >= len(df):
+                df = df.append({'X' : 0 , 'Y' : 0, 'Z' : 0, 'Pressure' : 0, 
+                                'GripAngle' : 0, 'Timestamp' : 0, 'ID' : 0,
+                                'Parkinsons' : parkinsonsFlag}, ignore_index = True)
+        zeroPadded.append(df)
+        print(df.iloc[9389,:])
+                
+        
+    #Convert to numpy array
+    numpyToSave = np.array([zeroPadded[i].values for i in range(len(dfs))])
+    print(numpyToSave.shape)
+    print(type(numpyToSave))
+    
+    #Split into u_train, y_train, and y_lag_train
+    
+    numpyToSave = np.transpose(numpyToSave, (1,2,0))
+    np.save('parkinsons.npy', numpyToSave)
+    
+#-------------------------------------------------------------
+    
+def transformData():
+    import mpu.ml
 
-numpyToSave = np.transpose(numpyToSave, (2,1,0))
+    print('Transforming ...')
+    
+    parkinsons = np.load('parkinsons.npy')
+    
+    #Create u_train
+    u_train = parkinsons[:,0:5,:]    
+    
+    #Create y_train
+    y = parkinsons[:,7,:]
+    
+    y_list = []
+    for pattern in np.transpose(y, (1,0)):
+        one_hot = np.array(mpu.ml.indices2one_hot(pattern, nb_classes=2))
+        y_list.append(one_hot)
 
-np.save('parkinsons.npy', numpyToSave)
+    y_train = np.array(y_list)
+    y_train = np.transpose(y_train, (1,2,0))    
+    
+    #Create y_lag_train
+    
+    y_lag_train = np.append(np.zeros((1,2,40)), y_train, axis = 0)[:-1]
 
-#Nore, can throw away Z and ID columns
+    print(u_train.shape)
+    print(y_train.shape)
+    print(y_lag_train.shape)
+
+    np.save('u_train.npy', u_train)
+    np.save('y_train.npy', y_train)
+    np.save('y_lag_train.npy', y_lag_train)
+
+
+#gatherData()
+transformData()
+
+#Note, can throw away Z and ID columns
